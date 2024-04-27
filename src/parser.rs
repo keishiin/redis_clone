5d4f78ce473ccdb32a97b8b2d558db6b9048ee01
keishiin <inferno2960@gmail.com>
2024-04-26 20:09:54 -0400

    chore: clean up parser and add tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,37 +1,30 @@
-fn parse_data_new(data: &[u8]) -> std::vec::Vec<&str> {
+use std::str::from_utf8;
+use std::vec::Vec;
+
+pub fn parse_data_new(data: &[u8]) -> Option<std::vec::Vec<&str>> {
     match data[0] as char {
-        '*' => parse_array(data),
-        '+' => parse_simple_string(data),
-        '$' => parse_bulk_string(data),
-        _ => panic!("not supported comomand"),
+        '*' => filter_data(data),
+        '+' => filter_data(data),
+        '$' => filter_data(data),
+        _ => None,
     }
 }
 
-fn parse_simple_string(buffer: &[u8]) -> std::vec::Vec<&str> {
-    let filerted_data = filter_data(buffer);
-    let cmd = filerted_data[0];
-    println!("parse simple string command: {}", cmd);
-    filerted_data
-}
-
-fn parse_array(buffer: &[u8]) -> std::vec::Vec<&str> {
-    let filerted_data = filter_data(buffer);
-    println!("parse array string: {:?}", filerted_data);
-    filerted_data
-}
-
-fn parse_bulk_string(buffer: &[u8]) -> std::vec::Vec<&str> {
-    let filerted_data = filter_data(buffer);
-    println!("parse bulk string: {:?}", filerted_data);
-    filerted_data
-}
-
-fn filter_data(buffer: &[u8]) -> std::vec::Vec<&str> {
-    let data = std::str::from_utf8(buffer).expect("unable to read from buffer");
-    data.trim_end_matches("\r\n")
+fn filter_data(buffer: &[u8]) -> Option<Vec<&str>> {
+    let data_in_utf8 = from_utf8(buffer).expect("unable to read from buffer");
+    let filtered: Vec<&str> = data_in_utf8
+        .trim_end_matches("\r\n")
         .split("\r\n")
         .filter(|&x| x.chars().all(|c| c.is_alphabetic()))
-        .collect()
+        .collect();
+
+    println!("{:?}", filtered);
+
+    if filtered.is_empty() {
+        None
+    } else {
+        Some(filtered)
+    }
 }
 
 #[cfg(test)]
@@ -41,22 +34,33 @@ mod tests {
 
     #[test]
     fn test_array_parse() {
-        let data = "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
-        let temp = parse_data_new(data.as_bytes());
-        assert_eq!(temp, ["SET", "key", "value"]);
+        let test_data = "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
+        let temp = parse_data_new(test_data.as_bytes());
+        let v = vec!["SET", "key", "value"];
+        assert_eq!(temp, Some(v));
+    }
+
+    #[test]
+    fn test_array_parse_multiple_key_values() {
+        let test_data = "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$3\r\nkey\r\n$5\r\nvalue\r\n";
+        let temp = parse_data_new(test_data.as_bytes());
+        let v = vec!["SET", "key", "value", "key", "value", "key", "value"];
+        assert_eq!(temp, Some(v));
     }
 
     #[test]
     fn test_simple_string_parse() {
-        let data = "+1\r\n$4\r\nping\r\n";
-        let temp = parse_data_new(data.as_bytes());
-        assert_eq!(temp, ["ping"]);
+        let test_data = "+1\r\n$4\r\nping\r\n";
+        let temp = parse_data_new(test_data.as_bytes());
+        let v = vec!["ping"];
+        assert_eq!(temp, Some(v));
     }
 
     #[test]
     fn test_bulk_string_parse() {
-        let data = "$6\r\nGET\r\n$5\r\nmykey\r\n";
-        let temp = parse_data_new(data.as_bytes());
-        assert_eq!(temp, ["GET", "mykey"]);
+        let test_data = "$6\r\nGET\r\n$5\r\nmykey\r\n";
+        let temp = parse_data_new(test_data.as_bytes());
+        let v = vec!["GET", "mykey"];
+        assert_eq!(temp, Some(v));
     }
 }
